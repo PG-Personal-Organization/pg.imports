7fd5efe96a200c1de2611ca489e4bde23ed34145
Patryk Grzywacz <patrykg198@gmail.com>
2025-07-27 18:15:32 +0200

    add integration test setup for Kafka imports in Spring module

---
+++ plugin.infrastructure.spring/src/test/java/pg/kafka/config/ImportsIntegrationTest.java
@@ -0,0 +1,26 @@
+package pg.kafka.config;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SpringBootTest(
+        classes = ImportsTestApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "spring.kafka.consumer.auto-offset-reset=earliest",
+                "spring.kafka.consumer.group-id=test-group",
+                "pg.kafka.bootstrap-server=${spring.embedded.kafka.brokers}",
+                "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
+        }
+)
+@ExtendWith(SpringExtension.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ImportsIntegrationTest {
+}
---
+++ plugin.infrastructure.spring/src/test/java/pg/kafka/config/ImportsTestApplication.java
@@ -0,0 +1,15 @@
+package pg.kafka.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@Import({
+        ImportsTestConfiguration.class
+})
+@SpringBootApplication
+public class ImportsTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImportsTestApplication.class, args);
+    }
+}
