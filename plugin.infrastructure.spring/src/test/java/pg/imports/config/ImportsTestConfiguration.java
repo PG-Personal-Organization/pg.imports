da2794fc990178c5e4dfe3742545764eb6c4b5d7
Patryk Grzywacz <patrykg198@gmail.com>
2025-08-04 18:39:37 +0200

    fix compiling and test run

--- plugin.infrastructure.spring/src/test/java/pg/imports/config/ImportsTestConfiguration.java
+++ plugin.infrastructure.spring/src/test/java/pg/imports/config/ImportsTestConfiguration.java
@@ -1,22 +1,10 @@
 package pg.imports.config;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfig;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Version;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import pg.lib.common.spring.config.CommonModuleConfiguration;
-
-import java.io.IOException;
 
 @Import({
         CommonModuleConfiguration.class
@@ -25,29 +13,4 @@ import java.io.IOException;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ImportsTestConfiguration {
 
-    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
-    private static final String DATABASE_NAME = "testdb";
-
-    @Bean
-    @Profile("mongo-test")
-    public MongodExecutable mongodExecutable() throws IOException {
-        MongodStarter starter = MongodStarter.getDefaultInstance();
-        MongodConfig mongodConfig = MongodConfig.builder()
-                .version(Version.Main.PRODUCTION)
-                .net(new Net(27017, false))
-                .build();
-        return starter.prepare(mongodConfig);
-    }
-
-    @Bean
-    @Profile("mongo-test")
-    public MongoClient mongoClient() {
-        return MongoClients.create(CONNECTION_STRING);
-    }
-
-    @Bean
-    @Profile("mongo-test")
-    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, DATABASE_NAME);
-    }
 }
